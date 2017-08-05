98f9d26d7b112304839d3be2a874608a67d60326
Madhavan Rajagopal Padmanabhan <mdhvnrp@gmail.com>
2017-08-04 19:28:08 -0500

    Initial Commit

---
+++ InfluenceMaximization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InfluenceMaximization
+//
+//  Created by Madhavan R.P on 8/4/17.
+//  Copyright © 2017 Madhavan R.P. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
